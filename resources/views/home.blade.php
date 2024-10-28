82ae640dc8ce12c7a3c1651b8e284b15c6480efa
syahiraqurrotunasyam <syahiraalya1933@gmail.com>
2024-10-28 10:38:23 +0700

    latihan 2

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -7,7 +7,7 @@
     <meta content="width=device-width, initial-scale=1.0, shrink-to-fit=no" name="viewport" />
     <link rel="icon" href="assets/img/kaiadmin/favicon.ico" type="image/x-icon" />
 
-    <!-- Fonts and icons -->
+    
     <script src="assets/js/plugin/webfont/webfont.min.js"></script>
     <script>
         WebFont.load({
